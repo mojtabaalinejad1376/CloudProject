bc17a60d5f784c5ff65fb686e2337fb62c36d8fd
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-08 18:46:20 +0330

    create register method & validate inputData

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -10,5 +10,32 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends BaseController
 {
-    
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'phone' => 'required|numeric|min:11|unique:App\Models\User',
+            'password' => 'required|min:8',
+            'confirm_password' => 'required|same:password'
+        ],
+            [
+                'first_name.required' => 'لطفا نام را وارد نماييد',
+                'first_name.string' => 'لطفا نام را به صورت رشته وارد نماييد',
+                'last_name.required' => 'لطفا نام خانوادگی را وارد نماييد',
+                'last_name.string' => 'لطفا نام خانوادگی را به صورت رشته وارد نماييد',
+                'phone.required' => 'لطفا شماره تلفن را وارد نماييد',
+                'phone.numeric' => 'لطفا شماره تلفن را به صورت عددی وارد نماييد',
+                'phone.min' => 'شماره تلفن نباید کمتر از 10 رقم باشد',
+                'phone.unique' => 'شماره تلفن قبلا ثبت شده است',
+                'password.required' => 'لطفا رمز عبور را وارد نماييد',
+                'password.min' => 'رمز عبور نبايد كمتر از 8 كاراكتر باشد',
+                'confirm_password.required' => 'لطفا تكرار رمز عبور را وارد نماييد',
+                'confirm_password.same' => 'رمز عبور مطابقت ندارد',
+            ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('خطا اعتبارسنجی', $validator->errors());
+        }
+    }
 }
