fcb83eb63fff887caeafb5c1bd86f0e4d2552826
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-08 18:32:57 +0330

    Initial Commit

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends BaseController
+{
+    
+}
