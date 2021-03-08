73804a293876036d3548d2e16ea27e69a2419f38
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-08 18:39:13 +0330

    define sendResponse Method in BaseController for return api response

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+
+        return response()->json($response, 200);
+    }
 }
