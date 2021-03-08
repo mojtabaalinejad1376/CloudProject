86bec341658583daa1832030334a2e474709b87e
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-08 18:55:06 +0330

    create login route

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ use App\Http\Controllers\API\RegisterController;
 //});
 
 Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
