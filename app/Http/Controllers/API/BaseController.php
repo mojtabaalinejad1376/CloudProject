37ff3882a7c1bddebc7d275f5a0445999f174630
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-08 18:40:05 +0330

    define sendError Method in BaseController for return api Error

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -23,4 +23,25 @@ class BaseController extends Controller
 
         return response()->json($response, 200);
     }
+
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+
+        return response()->json($response, $code);
+    }
 }
