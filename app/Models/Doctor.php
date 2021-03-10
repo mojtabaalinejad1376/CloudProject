d9d6ead85c900926b75ce906616cb79b16f325bf
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-10 14:36:59 +0330

    improve doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'nezam_number',
+        'city',
+        'speciality',
+        'degree',
+    ];
 }
