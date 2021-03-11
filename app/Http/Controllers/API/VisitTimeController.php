e01ba28df4939dc8470c5e40196b9bb596cdb152
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-11 15:36:59 +0330

    create visit time method & validate input request

--- app/Http/Controllers/API/VisitTimeController.php
+++ app/Http/Controllers/API/VisitTimeController.php
@@ -7,5 +7,26 @@ use Illuminate\Support\Facades\Validator;
 
 class VisitTimeController extends BaseController
 {
-
+    public function create_visit_time(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'year' => 'required|numeric',
+            'month' => 'required|numeric',
+            'day' => 'required|numeric',
+            'hour' => 'required|numeric',
+            'doctor_name' => 'required|string',
+        ],
+            [
+                'year.required' => 'لطفا سال را وارد نماييد',
+                'year.numeric' => 'لطفا سال را به صورت عددی وارد نماييد',
+                'month.required' => 'لطفا ماه را وارد نماييد',
+                'month.numeric' => 'لطفا ماه را به صورت عددی وارد نماييد',
+                'day.required' => 'لطفا روز را وارد نماييد',
+                'day.numeric' => 'لطفا روز را به صورت عددی وارد نماييد',
+                'hour.required' => 'لطفا ساعت را وارد نماييد',
+                'hour.numeric' => 'لطفا ساعت را به صورت عددی وارد نماييد',
+                'doctor_name.required' => 'لطفا نام پزشک را وارد نماييد',
+                'doctor_name.string' => 'لطفا نام کامل پزشک را به صورت رشته وارد نماييد',
+            ]);
+    }
 }
