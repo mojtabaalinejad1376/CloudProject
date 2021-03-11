9e743476597a7132a9c252ce2083fd4475105cba
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-11 16:01:19 +0330

    create visit time method for register visit time

--- app/Http/Controllers/API/VisitTimeController.php
+++ app/Http/Controllers/API/VisitTimeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Doctor;
+use App\Models\VisitTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -13,7 +15,7 @@ class VisitTimeController extends BaseController
             'year' => 'required|numeric',
             'month' => 'required|numeric',
             'day' => 'required|numeric',
-            'hour' => 'required|numeric',
+            'hour' => 'required|string',
             'doctor_name' => 'required|string',
         ],
             [
@@ -24,9 +26,28 @@ class VisitTimeController extends BaseController
                 'day.required' => 'لطفا روز را وارد نماييد',
                 'day.numeric' => 'لطفا روز را به صورت عددی وارد نماييد',
                 'hour.required' => 'لطفا ساعت را وارد نماييد',
-                'hour.numeric' => 'لطفا ساعت را به صورت عددی وارد نماييد',
+                'hour.string' => 'لطفا ساعت را به صورت عددی وارد نماييد',
                 'doctor_name.required' => 'لطفا نام پزشک را وارد نماييد',
                 'doctor_name.string' => 'لطفا نام کامل پزشک را به صورت رشته وارد نماييد',
             ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('خطا اعتبارسنجی', $validator->errors());
+        }
+
+        $doctor_id = Doctor::whereName($request['doctor_name'])->first();
+        if (is_null($doctor_id))
+            return $this->sendError('پزشکی با نام '. $request['doctor_name'] .' یافت نشد.', 'پزشکی با نام '. $request['doctor_name'] .' یافت نشد.');
+
+        $doctor_id = $doctor_id['id'];
+        $visit = VisitTime::create([
+           'year' => $request['year'],
+           'month' => $request['month'],
+           'day' => $request['day'],
+           'hour' => $request['hour'],
+           'doctor_id' => $doctor_id
+        ]);
+
+        return $this->sendResponse($visit, 'زمان ویزیت با موفقیت ثبت شد.');
     }
 }
