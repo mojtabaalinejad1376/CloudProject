88a96385cbf1fcb52d62cc181e0589a52b941fc3
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-03-11 15:11:19 +0330

    create visit time table for doctor

---
+++ database/migrations/2021_03_11_113759_create_visit_time_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitTimeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visit_time', function (Blueprint $table) {
+            $table->id();
+            $table->string('year');
+            $table->string('month');
+            $table->string('day');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('doctor_id')->references('id')->on('doctor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visit_time');
+    }
+}
